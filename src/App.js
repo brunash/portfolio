6d6903601ddf80f05a185563ba736d4bf79988ce
brunash <bobazso@yahoo.com>
2022-06-19 18:23:15 -0400

    added text animation

--- src/App.js
+++ src/App.js
@@ -13,67 +13,9 @@ import { motion } from 'framer-motion';
 
 
 function App() {
-
-  const [mousePosition, setMousePosition] = useState({
-    x: 0,
-    y: 5
-  });
-  const [cursorVariant, setCursorVariant] = useState("default");
-
-
-  useEffect(() => {
-    const mouseMove = e => {
-      setMousePosition({
-        x: e.clientX,
-        y: e.clientY
-      })
-    }
-
-    window.addEventListener("mousemove", mouseMove);
-
-    return () => {
-      window.removeEventListener("mousemove", mouseMove);
-    }
-  }, []);
-
-  const variants = {
-    default: {
-      x: mousePosition.x - 15,
-      y: mousePosition.y - 16,
-      backgroundColor: "yellow"
-    },
-    text: {
-      height: 75,
-      width: 75,
-      x: mousePosition.x - 50,
-      y: mousePosition.y - 50,
-      backgroundColor: "pink",
-      mixBlendMode: "overlay"
-    }
-  }
-
-  const textEnter = () => setCursorVariant("text");
-  const textLeave = () => setCursorVariant("default");
-
   return (
-    <div 
-    //  onMouseEnter = {
-    //    textEnter
-    //  }
-    //  onMouseLeave = {
-    //    textLeave
-    //  } 
-     >
+    <div>
       <BrowserRouter>
-      <motion.div
-      className = 'cursor'
-      variants = {
-        variants
-      }
-      animate = {
-        cursorVariant
-      }
-      />
         <NavBar/>
         <Routes>
         <Route path='/' exact element={<About/>}/>
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,45 +6,49 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 // import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { Link } from "react-router-dom";
-import { motion } from 'framer-motion';
-import { useEffect, useState } from 'react';
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import AnimatedLetters from "../AnimatedLetters/AnimatedLetters";
 
 let iconStyles = {
   fontSize: "50px",
 };
 
-
-
-
 const About = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 3000);
+  }, []);
 
   const [mousePosition, setMousePosition] = useState({
     x: 0,
-    y: 5
+    y: 5,
   });
   const [cursorVariant, setCursorVariant] = useState("default");
 
-
   useEffect(() => {
-    const mouseMove = e => {
+    const mouseMove = (e) => {
       setMousePosition({
         x: e.clientX,
-        y: e.clientY
-      })
-    }
+        y: e.clientY,
+      });
+    };
 
     window.addEventListener("mousemove", mouseMove);
 
     return () => {
       window.removeEventListener("mousemove", mouseMove);
-    }
+    };
   }, []);
 
   const variants = {
     default: {
       x: mousePosition.x - 15,
       y: mousePosition.y - 16,
-      backgroundColor: "yellow"
+      backgroundColor: "yellow",
     },
     text: {
       height: 75,
@@ -52,53 +56,62 @@ const About = () => {
       x: mousePosition.x - 50,
       y: mousePosition.y - 50,
       backgroundColor: "pink",
-      mixBlendMode: "overlay"
-    }
-  }
+      mixBlendMode: "overlay",
+    },
+  };
 
   const textEnter = () => setCursorVariant("text");
   const textLeave = () => setCursorVariant("default");
 
   return (
-    
-    <main main className = "container"
-    // onMouseEnter = {
-    //   textEnter
-    // }
-    // onMouseLeave = {
-    //   textLeave} 
-      >
+    <main
+      main
+      className="container"
+      onMouseEnter={textEnter}
+      onMouseLeave={textLeave}
+    >
       <motion.div
-      className = 'cursor'
-      variants = {
-        variants
-      }
-      animate = {
-        cursorVariant
-      }
+        className="cursor"
+        variants={variants}
+        animate={cursorVariant}
       />
-      <div div className = "container__information">
-        <section className = "container__about"
-        onMouseEnter = {
-          textEnter
-        }
-        onMouseLeave = {
-          textLeave
-        } 
+      <div div className="container__information">
+        <section
+          className="container__about"
+          onMouseEnter={textEnter}
+          onMouseLeave={textLeave}
         >
           <p>Hello, my name is</p>
-          <h1 >Slobodan Zaja</h1>
+
+          <div className="container__name">
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={[
+                "S",
+                "L",
+                "O",
+                "B",
+                "O",
+                "D",
+                "A",
+                "N",
+                " ",
+                "Z",
+                "A",
+                "J",
+                "A"
+              ]}
+              idx={15}
+            />
+          </div>
           <h2>I 'm a Software Developer</h2>
-          
+
           <h3>I'm a Full Stack Web Developer</h3>
         </section>
-        <div div className = "container__logos"
-        onMouseEnter = {
-          textEnter
-        }
-        onMouseLeave = {
-          textLeave
-        } 
+        <div
+          className="container__logos"
+          onMouseEnter={textEnter}
+          onMouseLeave={textLeave}
         >
           <a a href="https://www.linkedin.com/in/slobodanzaja">
             {" "}
@@ -110,7 +123,6 @@ const About = () => {
         </div>
       </div>
       <TechStack />
-     
     </main>
   );
 };
