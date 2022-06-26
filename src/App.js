d2befce11078e45c2d500b49c309d4554003e768
Slobodan Zaja <bobazso@yahoo.com>
2022-06-26 12:54:14 -0400

    adding sidebar

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
 import './styles/_mixins.scss';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <NavBar/>
+        {/* <NavBar/> */}
+        <Sidebar/>
         <Routes>
         <Route path='/' exact element={<About/>}/>
         <Route path='/projects' exact element={<Projects/>}/>
