61b3a9dce8ed4714d72f5fd41a41fef3650c4539
brunash <bobazso@yahoo.com>
2022-06-30 11:55:43 -0400

    changed contact page colors to better reflect the website styles

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,11 +1,32 @@
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
 
 export default function BasicButtons() {
+
+  const theme = createTheme({
+    status: {
+      danger: "#e53e3e",
+    },
+    palette: {
+      primary: {
+        main: "#a594f9ff",
+        darker: "#242038ff",
+      },
+      neutral: {
+        main: "#64748B",
+        contrastText: "#fff",
+      },
+    },
+  });
+
   return (
     <Stack spacing={2} direction="row">
-      <Button variant="outlined" type='submit'>Send Message</Button>
+      <ThemeProvider theme={theme}>
+      <Button variant="contained" type='submit' color='primary'>Send Message</Button>
+      </ThemeProvider>
     </Stack>
   );
 }
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,12 +3,28 @@ import React, { useRef } from 'react';
 import emailjs from 'emailjs-com';
 import Button from '../Button/Button'
 import { TextField } from '@mui/material';
-import { useEffect, useState } from 'react';
-
+import { createTheme, ThemeProvider} from '@mui/material/styles';
 
 
 const Contact = () => {
     const form = useRef();
+
+    const theme = createTheme({
+      status: {
+        danger: "#e53e3e",
+      },
+      palette: {
+        primary: {
+          main: "#a594f9ff",
+          darker: "#242038ff",
+        },
+        neutral: {
+          main: "#64748B",
+          contrastText: "#fff",
+        },
+      },
+    });
+
 
     const sendEmail = (e) => {
       e.preventDefault();
@@ -20,106 +36,69 @@ const Contact = () => {
       e.target.reset();
     };
 
-    const [mousePosition, setMousePosition] = useState({
-        x: 0,
-        y: 5
-    });
-    const [cursorVariant, setCursorVariant] = useState("default");
-
-
-    useEffect(() => {
-        const mouseMove = e => {
-            setMousePosition({
-                x: e.clientX,
-                y: e.clientY
-            })
-        }
-
-        window.addEventListener("mousemove", mouseMove);
-
-        return () => {
-            window.removeEventListener("mousemove", mouseMove);
-        }
-    }, []);
-
-    const variants = {
-        default: {
-            x: mousePosition.x - 15,
-            y: mousePosition.y - 16,
-            backgroundColor: "yellow"
-        },
-        text: {
-            height: 75,
-            width: 75,
-            x: mousePosition.x - 50,
-            y: mousePosition.y - 50,
-            backgroundColor: "pink",
-            mixBlendMode: "overlay"
-        }
-    }
-
-    const textEnter = () => setCursorVariant("text");
-    const textLeave = () => setCursorVariant("default");
 
     return (
-        <section className="contact">
-            <div className='contact__title--container'>
-            <h3 className='contact__title'>Send me a message</h3>
-            </div>
-            <form ref={form} onSubmit={sendEmail} className="contact__form">
-                <TextField 
-                className='contact__input'
-                required 
-                type='text' 
-                name='name' 
-                autoComplete="off"
-                label='Name'
-                sx={{
-                '& > :not(style)': { m: 1 },
+      <section className="contact">
+        <div className="contact__title--container">
+          <h3 className="contact__title">Send me a message</h3>
+        </div>
+        <form ref={form} onSubmit={sendEmail} className="contact__form">
+          <ThemeProvider theme={theme}>
+            <TextField
+              className="contact__input"
+              required
+              type="text"
+              name="name"
+              autoComplete="off"
+              label="Your name"
+              sx={{
+                "& > :not(style)": { m: 1 },
                 input: {
-                    color: 'white'
-                }
-                }}
-            
-                />
-                <TextField 
-                required
-                className='contact__input' 
-                type='email' 
-                name='email' 
-                autoComplete="off"
-                label='Email'
-                sx={{
-                '& > :not(style)': { m: 1 },
-                color: 'black',
+                  color: "primary",
+                },
+              }}
+            />
+            <TextField
+              required
+              className="contact__input"
+              type="email"
+              name="email"
+              autoComplete="off"
+              label="Your e-mail"
+              sx={{
+                "& > :not(style)": { m: 1 },
+                color: "primary",
                 input: {
-                    color: 'white',
-                    background: 'inherit'
-                }
-                }}
-                />
-               
-                <TextField 
-                multiline 
-                required 
-                className='contact__input'
-                type='text'
-                autoComplete="off"
-                sx={{
-                '& > :not(style)': { m: 1 },
-                color: 'primary',
+                  color: "primary",
+                  background: "neutral",
+                },
+              }}
+            />
+
+            <TextField
+              multiline
+              required
+              className="contact__input"
+              type="text"
+              autoComplete="off"
+              sx={{
+                "& > :not(style)": { m: 1 },
+                color: "primary",
                 input: {
-                    color: 'white',
-                }
-                }}
-                rows={4} name='message' placeholder='Type your message here'/>
-                {/* <button className='button' type='submit'>Send Message</button> */}
-                <div className='button__container'>
-                <Button className='button'></Button>
-                </div>
-            </form>
-        </section>
-    )
+                  color: "white",
+                },
+              }}
+              rows={4}
+              name="message"
+              placeholder="Type your message here"
+            />
+          </ThemeProvider>
+          <div className="button__container">
+            <Button className="button"></Button>
+          </div>
+        </form>
+      </section>
+    );
 }
 
 export default Contact;
